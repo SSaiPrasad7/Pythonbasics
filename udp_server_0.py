7599d4e34c6142206a113308755ee8865d5ac8f1
Prashanth Batchu <prashanth@batchu.net>
2020-07-26 09:49:06 -0400

    Updated log messages

--- udp_server_0.py
+++ udp_server_0.py
@@ -3,10 +3,12 @@ import time
 
 print("UDP SERVER SOCKET")
 udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_server.bind(("0.0.0.0", 2020))
-print("Waiting for Client to connect.....")
+server_ip = "0.0.0.0"
+udp_server.bind((("%s" % server_ip), 2020))
+print(f"{server_ip} -> Waiting for clients to connect.....")
 data, client_address = udp_server.recvfrom(1024)
-print(f"{client_address}")
-udp_server.sendto("Server->Hello".encode(),client_address)
+print(f"{server_ip} -> Received a message from {client_address}")
+print(f"{server_ip} -> Sending a response to  {client_address}")
+udp_server.sendto("Hello from Server".encode(),client_address)
 time.sleep(10)
 udp_server.close()
