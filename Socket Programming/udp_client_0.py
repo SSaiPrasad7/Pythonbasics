7d23f150ebaf7e3745317c4c9f25227bb4fa0e4e
saiprasad samudrala <samudralasai1999@gmail.com>
2020-07-27 19:52:20 +0530

    Added

--- Socket Programming/udp_client_0.py
+++ Socket Programming/udp_client_0.py
@@ -4,11 +4,11 @@ import time
 print("UDP CLIENT SOCKET")
 udp_client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 print("Client Connected.")
-client_ip = "127.0.0.3"
+client_ip = "127.0.0.1"
 data = "Hello"
-print(f"{client_ip}->{data}.")
+print(f"{client_ip}-> Sending {data} to ")
 udp_client.sendto(data.encode(), (("%s" % client_ip), 2020))
 time.sleep(10)
 data, server_address = udp_client.recvfrom(1024)
 returned_message = data.decode()
-print(returned_message)
+print(f"{client_ip} -> Received the following message {returned_message}")
