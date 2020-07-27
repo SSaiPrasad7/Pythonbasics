a0dbe4acddf7617d9de96c7f72503eac3700aa7d
saiprasad samudrala <samudralasai1999@gmail.com>
2020-07-27 22:33:24 +0530

    changed content

--- Socket Programming/tcp_client_0.py
+++ Socket Programming/tcp_client_0.py
@@ -2,9 +2,11 @@ import socket
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(("127.0.0.1",8080))
 print("Request Sent.")
+
 data=client.recv(1024)
 message=data.decode()
 print(message)
+
 message="Yes!!!"
 data=message.encode()
 client.send(data)
--- Socket Programming/tcp_server_0.py
+++ Socket Programming/tcp_server_0.py
@@ -5,11 +5,14 @@ my_server.listen()
 print("Waiting for Client to connect")
 connection_socket,address=my_server.accept()
 print("Client connected",address)
+
 message="Is it possible???"
 data=message.encode()
 connection_socket.send(data)
+
 data=connection_socket.recv(1024)
 message=data.decode()
 print(message)
+
 connection_socket.close()
 my_server.close()
