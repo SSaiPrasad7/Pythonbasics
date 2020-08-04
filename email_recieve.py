997486662de6487336d20525e2993ece604dcc2a
saiprasad samudrala <samudralasai1999@gmail.com>
2020-08-04 14:42:53 +0530

    added reciever

--- email_recieve.py
+++ email_recieve.py
@@ -1,17 +1,27 @@
 import email
 import imaplib
-imap_server=imaplib.IMAP4_SSL('imap.gmail.com')
+imap_server=imaplib.IMAP4_SSL('imap.gmail.com',993)
 email_address="samudralasai1999@gmail.com"
 password=input('Enter password: ')
 imap_server.login(email_address,password)
-imap_server.select('inbox')
-type,data=imap_server.search(None,'SUBJECT "test"')
-email_id=data[0]
-result,email_data=imap_server.fetch(email_id,'(RFC822)')
-raw_email=email_data[0][1].decode('utf-8')
-email_message=email.message_from_bytes(raw_email)
-for part in email_message.walk():
-    if part.get_content_type()=='text/plain':
-        body=part.get_payload(decode=True)
-        print(body)
+imap_server.select("inbox")
+_, search_data = imap_server.search(None, 'ALL')
+my_message = []
+for num in search_data[0].split():
+    email_data = {}
+    _, data = imap_server.fetch(num, '(RFC822)')
+    _, b = data[0]
+    email_message = email.message_from_bytes(b)
+    for header in ['subject', 'to', 'from', 'date']:
+        print("{}: {}".format(header, email_message[header]))
+        email_data[header] = email_message[header]
+    for part in email_message.walk():
+        if part.get_content_type() == "text/plain":
+            body = part.get_payload(decode=True)
+            email_data['body'] = body.decode()
+        elif part.get_content_type() == "text/html":
+            html_body = part.get_payload(decode=True)
+            email_data['html_body'] = html_body.decode()
+    my_message.append(email_data)
+    print(my_message)
 
